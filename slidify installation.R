e97f8830ee3f744d45b0172f58df93cf4b6f9d09
arcaravaggi <ar.caravaggi@gmail.com>
2017-07-27 11:37:28 +0100

    R script for install

---
+++ slidify installation.R
@@ -0,0 +1,8 @@
+library(devtools)
+install_github('ramnathv/slidify', force=TRUE)
+install_github('ramnathv/slidifyLibraries', force=TRUE)
+library(slidify)
+
+author('deck')
+
+slidify('index.Rmd')
